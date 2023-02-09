41123ce2dfc49cf41b092816cccefdad945e29cc
Huy Dinh <quanghuyqn@gmail.com>
2023-02-09 11:18:00 +0700

    add card crop sample

--- pages/Card-Crop.py
+++ pages/Card-Crop.py
@@ -7,12 +7,12 @@ import streamlit as st
 import numpy as np
 
 def main_loop():
-    st.title("Demo Tool: Crop ảnh thẻ")
-    st.text("Input: Ảnh chụp thẻ (ID Card/Drive license/Bank car/Student card..) bị nghiêng / xiên") 
-    st.text("Output: Ảnh crop thẻ và nắn thẳng")
+    st.set_page_config(page_title="Card Croping Tool")
+    st.title("Demo Tool: Card Croping")
+    st.subheader("Crop card từ ảnh chụp và chỉnh nghiêng ") 
+    st.image('pages/card-crop-sample.jpg',caption='Ảnh mẫu và kết quả xử lý')
 
-
-    updloaded_file = st.file_uploader("Upload ảnh", type=['jpg', 'png', 'jpeg'])
+    updloaded_file = st.file_uploader("Upload ảnh", type=['jpg', 'png', 'jpeg','tif','tiff'])
     if not updloaded_file:
         return None
     
@@ -20,12 +20,16 @@ def main_loop():
     src = cv2.imdecode(file_bytes, 1)
     cropedImg,debugImg=cardCrop(src)
     
-    st.text("Ảnh gốc:")
-    st.image(src,channels='BGR')
-    st.text("Ảnh định vị")
-    st.image(debugImg,channels='BGR')
-    st.text("Kết quả xử lý")
-    st.image(cropedImg,channels='BGR')
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.subheader("Ảnh gốc")
+        st.image(src,channels='BGR')
+    with col2:
+        st.subheader("Định vị")
+        st.image(debugImg,channels='BGR')
+    with col3:
+        st.subheader("Kết quả")
+        st.image(cropedImg,channels='BGR')
 
     #save uploaded image
     cv2.imwrite("./upload/"+updloaded_file.name,src)
